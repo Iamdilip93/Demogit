ea63e5c92fb27c753313ae84cf0f6ff03e384f11
Iamdilip93 <Dilipvr01@gmail.com>
2020-03-14 14:15:51 +0530

    Registration Code

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -4,9 +4,21 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 
-	
 	@Test
 	public void test1() {
 		System.out.println("Test1");
 	}
+
+	@Test
+	public void test2() {
+		System.out.println("New Result test 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("New Test Result test3 - reg");
+	}
+	
+	
+	
 }
